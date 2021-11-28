f5494fe733ccc5285d7e2c190f4ec04c7eac13dc
searjo <92434803+searjo@users.noreply.github.com>
2021-11-28 17:05:17 +0100

    Added insignificant_dummies_other function

--- Library/classification/binary_classify.py
+++ Library/classification/binary_classify.py
@@ -91,6 +91,30 @@ def scale_min2dum(train, test, dummycolumn1, dummycolumn2):
   test = pd.concat([test, futuredummy_test], axis=1)
   return train, test
 
+def insignificant_dummies_other(dummy_col, threshold):
+"""Turns insignificant dummies into other category
+    Parameters:
+           dummy_col: column to be changed
+           threshold: threshold=int that you want use for dummy removal
+         
+        Returns:
+        Dataframe with new dummies and other category
+        """
+    # removes the bind
+    dummy_col = dummy_col.copy()
+
+    # what is the ratio of a dummy in whole column
+    count = pd.value_counts(dummy_col) / len(dummy_col)
+
+    # cond whether the ratios are higher than the threshold
+    mask = dummy_col.isin(count[count > threshold].index)
+
+    # replace the ones which ratio is lower than the threshold by a special name
+    dummy_col[~mask] = "Other"
+
+    return pd.get_dummies(dummy_col, prefix=dummy_col.name)
+
+
 def run_cvp(regression, x, y, folds):
     """Runs cross_val_predict on your chosen data with chose number of k-folds
     Parameters:
